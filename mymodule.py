aa22b216dd2834dbb0932ffa9b3da0cbf7d78e4b
ronlin82 <60770133+ronlin82@users.noreply.github.com>
2020-02-07 14:44:21 +0800

    apikey

--- mymodule.py
+++ mymodule.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen
 import json
 
+api_key="XXXXXX"
+
 def get_weather(city):
     sock = urlopen("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=3f63ccf4a308a813a06606c1bc526a16")
     result = sock.read()                            
